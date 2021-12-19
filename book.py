30b8a80dfabd76cd14f7fe16506f895123bc04b0
AnnaSvechnikova <40806753+AnnSvechnikova@users.noreply.github.com>
2021-12-19 21:51:59 +0300

    Add files via upload
    Код и отчет для рубежного контроля № 2

---
+++ book.py
@@ -0,0 +1,7 @@
+class Book:
+    #книга
+    def __init__(self, id, title, year, lib_id):
+        self.id = id #id книги
+        self.title = title #название книги
+        self.year = year #год издания
+        self.lib_id = lib_id #id библиотеки
---
+++ library.py
@@ -0,0 +1,5 @@
+class Library:
+    #библиотека
+    def __init__(self, id, addr):
+        self.id = id #id библиотеки
+        self.addr = addr #адрес библиотеки
